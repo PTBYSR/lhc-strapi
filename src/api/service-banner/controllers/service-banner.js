0f56ab7e9d1cb69f1931e0145495c64b2c842b59
PTBYSR <paul.emechebe@gmail.com>
2023-10-06 17:56:07 +0100

    first commit

---
+++ src/api/service-banner/controllers/service-banner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * service-banner controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::service-banner.service-banner');
---
+++ src/api/service-banner/services/service-banner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * service-banner service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::service-banner.service-banner');
